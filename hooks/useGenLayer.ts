958b99dcc2667cc16ff39a841d7ba61b3dfd6583
emark-cloud <eakinleye97dami@gmail.com>
2025-12-13 19:10:03 +0100

    Update useGenLayer.ts

--- hooks/useGenLayer.ts
+++ hooks/useGenLayer.ts
@@ -1,13 +1,9 @@
 "use client";
 
-import { GenLayerClient } from "genlayer";
+import { GenLayerClient } from "@/lib/genlayerClient";
+
 
 export function useGenLayer() {
-  const apiKey = process.env.NEXT_PUBLIC_GENLAYER_API_KEY ?? "";
-
-  const client = new GenLayerClient({
-    apiKey,
-  });
-
-  return client;
+  const apiKey = process.env.NEXT_PUBLIC_GENLAYER_API_KEY || "";
+  return new GenLayerClient(apiKey);
 }
