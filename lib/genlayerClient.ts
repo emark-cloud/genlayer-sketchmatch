f77335107e2e3f087807bec2a014054a1a55decd
emark-cloud <eakinleye97dami@gmail.com>
2025-12-13 19:40:02 +0100

    Update genlayerClient.ts

--- lib/genlayerClient.ts
+++ lib/genlayerClient.ts
@@ -1,41 +1,41 @@
 export class GenLayerClient {
-  apiKey: string;
-
-  constructor(apiKey: string) {
-    this.apiKey = apiKey;
-  }
-
   async action(contract: string, action: string, args: any) {
     const res = await fetch("https://api.genlayer.com/v1/action", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
-        "X-Api-Key": this.apiKey,
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         contract,
         action,
-        args,
-      }),
+        args
+      })
     });
 
-    return await res.json();
+    if (!res.ok) {
+      throw new Error("GenLayer action failed");
+    }
+
+    return res.json();
   }
 
   async view(contract: string, view: string, args: any) {
     const res = await fetch("https://api.genlayer.com/v1/view", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
-        "X-Api-Key": this.apiKey,
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         contract,
         view,
-        args,
-      }),
+        args
+      })
     });
 
-    return await res.json();
+    if (!res.ok) {
+      throw new Error("GenLayer view failed");
+    }
+
+    return res.json();
   }
 }
